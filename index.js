8056b5cf77bdd246abf7522d0287d5dfec2ccac8
Victor Bjelkholm <victor@typeform.com>
2015-09-02 12:37:53 +0200

    Finish implementation

---
+++ index.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var fetch = require('node-fetch');
+var localtunnel = require('localtunnel');
+var bodyParser = require('body-parser')
+var moment = require('moment');
+var PromisePolyfill = require('es6-promise').Promise;
+var semver = require('semver');
+
+var app = express();
+
+if(semver.lt(process.version, '0.11.16')) {
+  throw new Error("You need to use nodejs version 0.11.16 or later");
+}
+
+// We want to serve our frontend from the /public folder
+app.use(express.static('public'));
+
+// Also, we need to be able to read the body in the request
+app.use(bodyParser.json());
+
+// API-TOKEN to use when creating the typeform
+var API_TOKEN = process.env.TYPEFORM_API_TOKEN;
+
+// Which version of the I/O API we want to use
+var API_VERSION = 'v0.4'
+
+// A public url where the webhook can find this application
+var PUBLIC_URL = null;
+
+// Which endpoint the webhooks will use
+var SUBMIT_ENDPOINT = '/submit_answer';
+
+// Which port to run the application on
+var SERVER_PORT = 3000;
+
+// Array with all the answers we've received so far
+var ANSWERS = [];
+
+// If there is no API_TOKEN set, we need to crash to prevent any auth errors
+if(API_TOKEN === undefined) {
+  throw new Error('You need to set the environment variable TYPEFORM_API_TOKEN for this application to run');
+}
+
+// This will set the PUBLIC_URL to a domain that will map to your local computer
+var tunnel = localtunnel(SERVER_PORT, function(err, tunnel) {
+    if (err) {
+      throw new Error(err);
+    }
+    PUBLIC_URL = tunnel.url;
+});
+
+// Helper method to create a new typeform
+function create_typeform(form) {
+  return new PromisePolyfill(function(resolve) {
+    fetch('https://api.typeform.io/'+API_VERSION+'/forms', {
+        method: 'POST',
+        headers: {
+          'X-API-TOKEN': API_TOKEN
+        },
+        body: JSON.stringify(form)
+      }).then(function(response) {
+        return response.json();
+      }).then(resolve);
+  });
+}
+
+// Endpoint that get hit by the frontend when clicking on "Create your typeform"
+app.post('/create_form', function (req, res) {
+  // Take the name from the request body
+  var name = req.body.name;
+
+  // The form we want to create
+  var form = {
+    title: 'Test Form',
+    // This is the endpoint we'll use to receive the results via webhooks
+    webhook_submit_url: PUBLIC_URL + SUBMIT_ENDPOINT,
+    fields: [
+      {
+        type: 'yes_no',
+        // Dynamic question with the users name
+        question: 'Is your name really ' + name + '???'
+      }
+    ]
+  };
+
+  // Create our form and reply to the request we got
+  create_typeform(form).then(function(created_form) {
+    res.send(created_form);
+  });
+});
+
+// Endpoint that get hit by Typeform I/O when a form have been submitted
+app.post(SUBMIT_ENDPOINT, function(req, res) {
+  var results = req.body;
+  // Let's add the time when we received the results as well
+  results.time = moment().format('H:mm:ss');
+  // Add the full results to our ANSWERS storage
+  ANSWERS.push(results)
+});
+
+// Endpoint that get hit by the frontend every second to update the list of results
+app.get('/answers', function(req, res) {
+  res.send(ANSWERS);
+});
+
+// Start express
+var server = app.listen(SERVER_PORT, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Server running on http://' + host + ':' + port);
+});
+
---
+++ public/app.js
@@ -0,0 +1,125 @@
+// Function that gets called in a interval to show current time
+function set_new_time($time) {
+  $time.innerHTML = 'Current time: ' + moment().format('H:mm:ss');
+}
+
+// Helper method to return json from a response
+function responseToJson(response) {
+  return response.json();
+}
+
+// Helper method to get the answers from our backend
+function get_answers() {
+  return new Promise(function(resolve) {
+    fetch('/answers').then(responseToJson).then(resolve);
+  });
+}
+
+// Helper method to create the text containing all the results
+function create_list_of_answers(answers) {
+    var list = '';
+    for(var i = 0; i < answers.length; i++) {
+        // Take the answer of our first question from the form answers
+        var said_yes = answers[i].answers[0].value;
+        // Take the time as well
+        var time = answers[i].time;
+        var answer = null;
+        if(said_yes) {
+          answer = 'yes';
+        } else {
+          answer = 'no';
+        }
+        // Concatenate together the lines we want to show
+        list = list + time + ' - You said ' + answer + '<br/>'
+    }
+    // If list is a empty string, we don't have any answers yet
+    if(list === '') {
+      list = 'When you answered your form, the results will show up here...'
+    }
+    return list;
+}
+
+// Helper method to render the answers
+function render_answers(answers, $answers) {
+  $answers.innerHTML = create_list_of_answers(answers);
+}
+
+// Helper method to get the form render link from a form
+function get_render_link_from_form(form) {
+  var to_return = null;
+  // We need to iterate through all the links of the form
+  form._links.forEach(function(link) {
+    // If the rel property is "form_render", we found the right link to show
+    // the form
+    if(link.rel === 'form_render') {
+      to_return = link.href;
+    }
+  });
+  return to_return;
+}
+
+// Helper method to create a form based on a name
+function create_form_with_name(name) {
+  return new Promise(function(resolve) {
+    // The body in the request, set the name
+    var body = {
+      name: name
+    };
+    // Options for the request
+    var fetch_options = {
+      // We're creating a form, hence a POST request
+      method: 'POST',
+      headers: {
+        // Set the content-type to make sure our backend understands that
+        // we're sending JSON
+        'Content-Type': 'application/json'
+      },
+      // Set the body
+      body: JSON.stringify(body)
+    };
+    // Make the actual request to the /create_form endpoint
+    fetch('/create_form', fetch_options).then(responseToJson).then(resolve);
+  })
+}
+
+// Helper method to show a form
+function show_form(form, $render_area) {
+  // Change the src property of the iframe where we want to see the form
+  $render_area.src = get_render_link_from_form(form);
+}
+
+// Function that gets called when the dom is ready for us
+function start_app() {
+  $input_name = document.getElementById('name');
+  $create_form_button = document.getElementById('create_form');
+  $form_iframe = document.getElementById('form');
+  $answers = document.getElementById('answers');
+  $time = document.getElementById('time');
+
+  // When user clicks on the create form button
+  $create_form_button.addEventListener('click', function() {
+    var name = $input_name.value;
+    if(name === '') {
+      alert('Name cannot be empty!');
+    } else {
+      // If we got a name, create the form
+      create_form_with_name(name).then(function(form) {
+        // And then show it in the panel to the right
+        show_form(form, $form_iframe);
+      });
+    }
+  });
+
+  setInterval(function() {
+    // Every second, update the clock
+    set_new_time($time);
+    // And try to get the answers from our backend
+    get_answers().then(function(answers) {
+      // And render them in the bottom left panel
+      render_answers(answers, $answers);
+    });
+  }, 1000);
+}
+
+// When we're ready, start the app
+document.addEventListener('DOMContentLoaded', start_app);
